e19d6841b27efc46d2f3892d279bfdcd5fdb1507
Sébastien Lundi <slundi@stamus-networks.com>
2026-04-02 17:08:24 +0200

    feat: setup worker(s) from configuration or CPU count

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,57 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+use crate::config::Config;
+use tokio::runtime::Builder;
+
 mod cli;
 mod config;
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn run(config: Config) {
+    println!("Dgaard starting on {}", config.server.listen_addr);
+}
+
+fn start_with_single_worker(config: Config) -> Result<(), Box<dyn std::error::Error>> {
+    let runtime = Builder::new_current_thread()
+        .thread_stack_size(config.server.runtime.stack_size)
+        .max_blocking_threads(config.server.runtime.max_blocking_threads)
+        .build()?;
+    runtime.spawn(async move { run(config) });
+    Ok(())
+}
+fn start_with_workers(config: Config, cpus: usize) -> Result<(), Box<dyn std::error::Error>> {
+    let runtime = Builder::new_multi_thread()
+        .worker_threads(cpus)
+        .thread_name_fn(|| {
+            static ATOMIC_ID: AtomicUsize = AtomicUsize::new(0);
+            let id = ATOMIC_ID.fetch_add(1, Ordering::SeqCst);
+            format!("dgaard-{}", id)
+        })
+        .thread_stack_size(config.server.runtime.stack_size)
+        .max_blocking_threads(config.server.runtime.max_blocking_threads)
+        .build()?;
+    for _ in 0..cpus {
+        let cfg = config.clone();
+        runtime.spawn(async move { run(cfg) });
+    }
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts = cli::parse();
 
     let config_path = config::discover_path(opts.config.as_deref()).ok_or("Configuration file not found. Please provide one via --config or place it in /etc/dgaard/config.toml")?;
 
     let config = config::Config::load(&config_path)?;
-    println!("Dgaard starting on {}", config.server.listen_addr);
 
-    Ok(())
+    let cpus = match config.server.runtime.worker_threads {
+        config::WorkerThreads::Auto => num_cpus::get(),
+        config::WorkerThreads::Count(n) => n,
+    };
+
+    println!("Preparing dgaard runtime with {} thread(s)", cpus);
+    if cpus == 1 {
+        Ok(start_with_single_worker(config)?)
+    } else {
+        Ok(start_with_workers(config, cpus)?)
+    }
 }
